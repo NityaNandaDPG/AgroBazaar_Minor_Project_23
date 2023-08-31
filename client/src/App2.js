7ce825d9cfdfe76037ce493edcdaeff0bdc8f9ba
Nityananda Adhikary <nnadkbdhupguri@gmail.com>
2023-09-01 02:47:41 +0530

    App ready for home page

---
+++ client/src/App2.js
@@ -0,0 +1,21 @@
+// import logo from './logo.svg';
+import Header from './components/Header';
+import './App.css';
+import MarketPrice from './components/MarketPrice';
+import Footer from './components/Footer';
+
+function App() {
+  return (
+    
+    <div>
+      <Header/>
+      <main>
+        <MarketPrice/>
+      </main>
+      <Footer/>
+
+    </div>
+  );
+}
+
+export default App;
