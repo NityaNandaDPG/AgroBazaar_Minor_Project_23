2f58ebf2211267999061477ecd3c1e61bbc00697
NityaNanda Adhikary <50585942+NityaNandaDPG@users.noreply.github.com>
2023-11-03 17:41:57 +0530

    Product same are now shown in Cart with Quantity and Remove Option

--- client/src/components/Basket.js
+++ client/src/components/Basket.js
@@ -1,26 +1,47 @@
-import React, {useEffect, useState } from 'react';
-import { useSelector} from "react-redux";
+import React, { useEffect, useState } from 'react';
+import { useSelector } from "react-redux";
 import axios from 'axios';
 
 const Basket = () => {
-    const id= useSelector((state) => state.user._id);
+    const id = useSelector((state) => state.user._id);
     const [cart, setCart] = useState([]);
+    const [productDetails, setProductDetails] = useState({});
 
     useEffect(() => {
         axios.get(`http://localhost:8082/add2cart/${id}`)
-        .then(res => {
-            setCart(res.data);
-        })
-        .catch(error => {
-            console.error('Failed to fetch cart products:', error.message);
-        });
-        }, [cart]);
-
+            .then(res => {
+                setCart(res.data);
+                // Fetch product details for each item in the cart
+                res.data.forEach(item => {
+                    axios.get(`http://localhost:8082/products/all/${item._id}`)
+                        .then(response => {
+                            // Update productDetails with the fetched details
+                            setProductDetails(prevProductDetails => ({
+                                ...prevProductDetails,
+                                [item._id]: response.data,
+                            }));
+                        })
+                        .catch(error => {
+                            console.error('Failed to fetch product details:', error.message);
+                        });
+                });
+            })
+            .catch(error => {
+                console.error('Failed to fetch cart products:', error.message);
+            });
+    }, [cart,id]);
 
     const removeFromCart = (productId) => {
         axios.delete(`http://localhost:8082/add2cart/${id}/remove/${productId}`)
             .then(response => {
                 console.log("Remove successfully!");
+                // Remove the item from cart and productDetails
+                setCart(cart.filter(item => item._id !== productId));
+                setProductDetails(prevProductDetails => {
+                    const newProductDetails = { ...prevProductDetails };
+                    delete newProductDetails[productId];
+                    return newProductDetails;
+                });
             })
             .catch(error => {
                 console.error('Failed to remove product from cart:', error.message);
@@ -29,27 +50,15 @@ const Basket = () => {
 
     const updateQuantity = (productId, newQuantity) => {
         axios.put(`http://localhost:8082/add2cart/${id}/update/${productId}/${newQuantity}`)
-        .then(response => {
-            const updatedCart = response.data.cart;
-            setCart(updatedCart);
-            console.log("Quantity Updated successfully!");
-        })
-        .catch(error => {
-            console.error('Failed to update quantity:', error.message);
-        });
-    };
-
-
-    function getProduct(productId) {
-        axios.get(`http://localhost:8082/products/all/${productId}`)
             .then(response => {
-                return response.data;
+                const updatedCart = response.data.cart;
+                setCart(updatedCart);
+                console.log("Quantity Updated successfully!");
             })
-            .catch (error => {
-                console.error('Failed to fetch product name:', error.message);
-                return 'Product Name Not Found';
+            .catch(error => {
+                console.error('Failed to update quantity:', error.message);
             });
-    }
+    };
 
     if (!cart) {
         return <p>Loading...</p>;
@@ -59,32 +68,28 @@ const Basket = () => {
         return <p>Your cart is empty.</p>;
     }
 
-
     return (
         <div className="bg-white rounded p-4 shadow-md">
             <h2 className="text-xl font-semibold mb-4">Shopping Cart</h2>
             <ul>
-            {cart.map((item,i) => (
-                <li key={i}>
-                    <div className="flex justify-between items-center mb-2">
-                        <span>{item._id}</span>
-                        {/* <span>{productName}</span> */}
-                        {/* <span>{getProductName(item._id)}</span> */}
-                        <div className="flex items-center space-x-2">
-                            <button className="text-red-500"
-                            onClick={() => removeFromCart(item._id)}>
-                                Remove
-                            </button>
-                            <input
-                                type="number"
-                                value={item.quantity}
-                                onChange={(e) => updateQuantity(item._id, e.target.value)}
-                                className="w-16 p-2 border border-gray-300 rounded"
-                            />
+                {cart.map((item, i) => (
+                    <li key={i}>
+                        <div className="flex justify-between items-center mb-2">
+                            <span>{productDetails[item._id]?.name || 'Product Name Not Found'}</span>
+                            <div className="flex items-center space-x-2">
+                                <button className="text-red-500" onClick={() => removeFromCart(item._id)}>
+                                    Remove
+                                </button>
+                                <input
+                                    type="number"
+                                    value={item.quantity}
+                                    onChange={(e) => updateQuantity(item._id, e.target.value)}
+                                    className="w-16 p-2 border border-gray-300 rounded"
+                                />
+                            </div>
                         </div>
-                    </div>
-                </li>
-            ))}
+                    </li>
+                ))}
             </ul>
         </div>
     );
